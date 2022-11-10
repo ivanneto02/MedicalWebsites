04f955a86ba44d55c44e7a9d199a587072328338
ivanneto02 <ivan.a.neto@hotmail.com>
2022-11-10 10:04:49 -0800

    Adding all starting files

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+DATA_PATH = os.environ["DATA_PATH"]
+DATA_NAME = os.environ["DATA_NAME"]
+N_ROWS = os.environ["N_ROWS"]
---
+++ main.py
@@ -0,0 +1,10 @@
+from .config import *
+
+import pandas as pd
+
+def main():
+    # Run the program
+    pass
+
+if __name__ == "__main__":
+    main()
